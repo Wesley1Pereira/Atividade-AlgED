891ae5b35b85ff1e9b0a08a692afb9a660e72901
Wesley1Pereira <wespereira2020@gmail.com>
2022-09-25 20:27:31 -0300

    Atividades Avaliativa

---
+++ Atividade 01/ATIVIDADE01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+	int n,c,r;
+	
+	printf("Coloque um numero entre 1 e 10");
+	scanf("%d, &n");
+	
+	for (c=1; c <= 10; c++){
+		r=n*c;
+		printf("\n %d x %2d = %3d",n,c,r);
+		
+	}
+	return 0;
+}
